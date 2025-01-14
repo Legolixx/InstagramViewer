7e397b5b9c7adbd2a38b26e5e44ae0d4e25689ec
Dev2025 <victorfazekas3@gmail.com>
2025-01-14 17:51:32 -0300

    enhance IntegrationCard component with layout and connect button

--- src/app/(protected)/dashboard/[slug]/integrations/_components/integration-card/index.tsx
+++ src/app/(protected)/dashboard/[slug]/integrations/_components/integration-card/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 type Props = {
@@ -8,7 +9,23 @@ type Props = {
 };
 
 function IntegrationCard({ description, icon, strategy, title }: Props) {
-  return <div>IntegrationCard</div>;
+  return (
+    <div className="border-2 border-[#3352CC] rounded-2xl gap-x-5 p-5 flex items-center justify-between">
+      {icon}
+      <div className="flex flex-col flex-1">
+        <h3 className="text-xl">{title}</h3>
+        <p className="text-[#909090] text-base w-full md:w-10/12 xl:w-8/12 2xl:w-6/12">
+          {description}
+        </p>
+      </div>
+      <Button
+        className="bg-gradient-to-br text-white rounded-full text-lg from-[#3352CC]
+      font-medium to-[#1C2D70] hover:opacity-70 transition duration-100"
+      >
+        Connect
+      </Button>
+    </div>
+  );
 }
 
 export default IntegrationCard;
